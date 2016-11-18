217ca7992bfaae6afb2bf554469fbc4408ece79d
Daniel McSorley <mcsorley.1@gmail.com>
2016-11-17 19:56:40 -0500

    add node-notifications

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const {app, Menu, Tray} = require('electron')
+const notifier = require('node-nodifier')
 
 let tray = null
-app.on('ready', () => {
+
+const createTray = () => {
   tray = new Tray('tray-icon.png')
   const contextMenu = Menu.buildFromTemplate([
     {label: 'Enterprise Happiness', type: 'normal'}
   ])
   tray.setToolTip('Enterprise Happiness')
   tray.setContextMenu(contextMenu)
+}
+
+app.on('ready', () => {
+  createTray()
+  notifier.notify({
+    title: 'Enterprise happiness',
+    message: 'Would you recommend your employer to a friend?'
+  })
 })
