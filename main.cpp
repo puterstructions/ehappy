111f1309283e57ee7a0822222fb3550aa56bb90f
n8behavior <mike@thesandmans.com>
2017-04-13 09:56:15 -0400

    clean-up and reorg some shit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,46 @@
-#include "happysettings.h"
 #include <QApplication>
+
+#ifndef QT_NO_SYSTEMTRAYICON
+
+#include <QMessageBox>
+#include "window.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    HappySettings w;
-    w.show();
+    Q_INIT_RESOURCE(systray);
 
-    return a.exec();
+    QApplication app(argc, argv);
+
+    if (!QSystemTrayIcon::isSystemTrayAvailable()) {
+        QMessageBox::critical(0, QObject::tr("Systray"),
+                              QObject::tr("I couldn't detect any system tray "
+                                          "on this system."));
+        return 1;
+    }
+    QApplication::setQuitOnLastWindowClosed(false);
+
+    Window window;
+    window.show();
+    return app.exec();
 }
+
+#else
+
+#include <QLabel>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QString text("QSystemTrayIcon is not supported on this platform");
+
+    QLabel *label = new QLabel(text);
+    label->setWordWrap(true);
+
+    label->show();
+    qDebug() << text;
+
+    app.exec();
+}
+
+#endif
